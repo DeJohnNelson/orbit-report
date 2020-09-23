45264a1c26e946d5e04f5afc090e868345483b6a
DeJohn Nelson <dejohn.nelson1998@gmail.com>
2020-09-23 16:04:27 -0500

    almost done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,17 @@ export class AppComponent {
        new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
        new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
     ];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+    window.fetch(satellitesUrl).then(function(response) {
+      response.json().then(function(data) {
+        let fetchedSatellites = data.satellites;
+        for(let i =0; i > fetchedSatellites.length; i++) {
+         let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+             this.sourceList.push(satellite);
+        };
+      }.bind(this));
+    }.bind(this));
  }
 }
 
